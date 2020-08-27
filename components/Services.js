6b2bc2055371318450ebfa5414d8a39cc45135fd
jeynetwork <jeymwira@gmail.com>
2020-08-27 12:44:09 +0300

    basicComponents

---
+++ components/Services.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+
+export default function Auth(){
+    return(
+        <View>
+            <Text style={styles.Text} >
+                Here is my services page
+            </Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    Text:{
+        fontStyle:'bold'
+    }
+})
